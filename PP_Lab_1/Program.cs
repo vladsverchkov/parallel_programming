44bc5f8cdb6a473f9e882ebaec3a9eb17f8cdc5a
vladsverchkov <vladenigmatic@gmail.com>
2020-10-15 15:59:24 +0300

    Додано функцію створення списку з великою кількістю випадкових чисел+ вимір часу виконання операції

--- PP_Lab_1/Program.cs
+++ PP_Lab_1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PP_Lab_1
@@ -10,6 +12,23 @@ namespace PP_Lab_1
     {
         static void Main(string[] args)
         {
+            
         }
+
+        static void InitRandomListData(ref List<int> numbers, int count)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+           
+            var random = new Random();
+
+            for (var i = 0; i < count; i++)
+                numbers.Add(random.Next(99999));
+          
+            watch.Stop();
+
+            Console.WriteLine($"InitRandomListData => Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+       
     }
 }
