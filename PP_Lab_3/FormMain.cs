d79cbde1bb4df4bfdcf2ecaa4bda43efd7405619
vladsverchkov <vladenigmatic@gmail.com>
2020-11-12 02:05:32 +0200

    Реалізація функціоналу для кнопки генерації частинок

--- PP_Lab_3/FormMain.cs
+++ PP_Lab_3/FormMain.cs
@@ -42,12 +42,50 @@ namespace PP_Lab_3
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Random rnd = new Random();
+            try
+            {
+                if (numericUpDown1.Value == 0)
+                    throw new ArgumentException();
+                p = new Particle[(int)numericUpDown1.Value - 1];
+            }
+            catch
+            {
+                MessageBox.Show("Перевірте введені данні", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
+                return;
+            }
+            for (int i = 0; i < p.Length; i++)
+                p[i] = new Particle();
+            pc = new ParticleWithCoord();
+
+
+            int midle_speed = Convert.ToInt32(Math.Sqrt(3 * 0.013 * trackBar1.Value));
+
+            for (int j = 0; j < p.Length; j++)
+            {
+                p[j].SetMaxSpeed = midle_speed + 1;
+                p[j].X = rnd.Next(pictureBox1.Width);
+                p[j].Y = rnd.Next(pictureBox1.Height);
+                p[j].X_Speed = rnd.Next(-midle_speed, midle_speed);
+                p[j].Y_Speed = rnd.Next(-midle_speed, midle_speed);
+
+                drawPoint(Convert.ToInt32(p[j].X), Convert.ToInt32(p[j].Y), pictureBox1);
+
+            }
+            pc.SetMaxSpeed = midle_speed + 1;
+            pc.X = rnd.Next(700);
+            pc.Y = rnd.Next(350);
+            pc.X_Speed = rnd.Next(-midle_speed, midle_speed);
+            pc.Y_Speed = rnd.Next(-midle_speed, midle_speed);
+
+            drawPoint(Convert.ToInt32(pc.X), Convert.ToInt32(pc.Y), pictureBox1);
+            button2.Enabled = true;
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
