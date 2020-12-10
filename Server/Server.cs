3a6dd109e75bc31fba3d5374abd1d227430b56e9
vladsverchkov <vladenigmatic@gmail.com>
2020-12-10 03:03:55 +0200

    Реалізація функції вивантаження даних з серверу. Реалізація менеджеру даних, котрий опрацьовує наступні дії: візуалізація загального повідомлення, візуалізація особистого повідомлення, реєстрація у чаті під нікнеймом, відображення списку юзерів

--- Server/Server.cs
+++ Server/Server.cs
@@ -182,6 +182,141 @@ namespace NetWork.Server
             Thread.CurrentThread.Abort();
         }
 
+        public static void DataOut(int PacketType, string[] sendData)
+        {
+            var content = Tuple.Create(PacketType, sendData);
+            byte[] packet = Packet.Pack(content);
+
+            try
+            {
+                if (started && users.Count > 0)
+                {
+                    foreach (UserData user in users)
+                        user.clientSocket.Send(packet);
+                }
+                else
+                    serverForm.ChatWindow("No Clients to send to", colorAlert);
+            }
+            catch (SocketException sx)
+            {
+                serverForm.ChatWindow("SocketException: " + sx, colorAlert);
+            }
+        }
+
+        public static void DataOut(Socket socket, int PacketType, string[] sendData)
+        {
+            try
+            {
+                if (started && users.Count > 0)
+                {
+                    var content = Tuple.Create(PacketType, sendData); 
+                    socket.Send(Packet.Pack(content)); 
+                }
+                else
+                    serverForm.ChatWindow("No Clients to send to", colorAlert);
+            }
+            catch (SocketException sx)
+            {
+                serverForm.ChatWindow("SocketException: " + sx, colorAlert);
+            }
+        }
+
+        public static void DataOutExcludeSender(Socket socket, int PacketType, string[] sendData)
+        {
+            var content = Tuple.Create(PacketType, sendData);
+            byte[] pakcet = Packet.Pack(content);
+
+            try
+            {
+                if (started && users.Count > 0)
+                {
+                    foreach (UserData user in users)
+                    {
+                        if (user.clientSocket != socket)
+                            user.clientSocket.Send(pakcet); 
+                    }
+                }
+                else
+                    serverForm.ChatWindow("No Clients to send to", colorAlert);
+            }
+            catch (SocketException sx)
+            {
+                serverForm.ChatWindow("SocketException: " + sx, colorAlert);
+            }
+        }
+
+        static void DataManager(UserData user, Tuple<int, string[]> content) 
+        {
+            if (content.Item1 >= 0 && content.Item1 <= 2)
+            {
+                switch (content.Item1)
+                {
+                    case 0: //чат
+                        if (user.registerd && content.Item2.Length == 1)
+                        {
+                            serverForm.ChatWindow(user.name + ": " + content.Item2[0], colorChat);
+
+                            var newContent = new string[2];
+                            newContent[0] = user.name;
+                            newContent[1] = content.Item2[0];
+
+                            DataOutExcludeSender(user.clientSocket, 0, newContent); //sends
+                        }
+                        break;
+
+                    case 1: //конкретний чат
+                        if (user.registerd && content.Item2.Length == 2)
+                        {
+                            if (user.name != content.Item2[0])
+                            {
+                                serverForm.ChatWindow(user.name + " -> " + content.Item2[0] + ": " + content.Item2[1], colorSpecificChat);
+
+                                var specificChat = new string[2];
+                                specificChat[0] = user.name;
+                                specificChat[1] = content.Item2[1];
+
+                                Socket toClient = GetClientSocket(content.Item2[0]);
+
+                                if (toClient != null)
+                                    DataOut(toClient, 1, specificChat); //sends
+                            }
+                            else
+                            {
+                                DisconnectClient(client, "incorrect packet sent");
+                            }
+                        }
+                        break;
+
+                    case 2: //реєстрація
+                        if (IsBand(((IPEndPoint)user.clientSocket.RemoteEndPoint).Address))
+                        {
+                            DisconnectClient(user, "you are band");
+                        }
+                        else
+                        if (CheckIfNamesTaken(content.Item2[0]))
+                        {
+                            DisconnectClient(user, "name already taken");
+                        }
+                        else
+                        {
+                            user.name = content.Item2[0];
+                            user.registerd = true;
+                            serverForm.UserList(user.name, false);
+                            serverForm.ChatWindow(user.name + " connected", colorConnect);
+
+                            var userName = new string[1];
+                            userName[0] = user.name;
+                            DataOutExcludeSender(user.clientSocket, 3, userName);
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                DisconnectClient(user, "disconnected for not being registerd");
+            }
+        }
+
         public static UserData GetClient(string name)
         {
             for (int i = 0; i < users.Count; i++)
